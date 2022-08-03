92620d4c7e338f1a09849f918938266e77027dd6
Albert Canales Ros <albertcanalesros@gmail.com>
2022-08-03 16:10:59 +0200

    Added the rest of the parameters

--- drawer.py
+++ drawer.py
@@ -10,7 +10,7 @@ books = [
 ]
 
 def draw(path, c):
-    dwg = svgwrite.Drawing(path, (600, 5100), viewBox="0 0 158.75 1349.3751")
+    dwg = svgwrite.Drawing(path, (c.canvas_size_x, c.canvas_size_y), viewBox="0 0 158.75 1349.3751")
     dwg = draw_grid(dwg, c)
     for book in books:
         dwg = draw_book(dwg, book, c)
@@ -18,30 +18,31 @@ def draw(path, c):
 
 def draw_grid(dwg, c):
     # Vertical lines
-    dwg.add(dwg.line((15.875, 3), (15.875, 5100), stroke='#e5e5e5', stroke_width=0.264))
-    dwg.add(dwg.line((42.33, 3), (42.33, 5100), stroke='#e5e5e5', stroke_width=0.264))
+    dwg.add(dwg.line((c.timeline_start_x, c.timeline_start_y), (c.timeline_start_x, c.canvas_size_y), stroke=c.timeline_line_color, stroke_width=c.timeline_line_width))
+    dwg.add(dwg.line((c.timeline_end_x, c.timeline_start_y), (c.timeline_end_x, c.canvas_size_y), stroke=c.timeline_line_color, stroke_width=c.timeline_line_width))
 
     # Horizontal lines
+
     for i in range(5):
-        dwg.add(dwg.line((10.6, 10.6 + 6.75 + 53*i), (10.6+31.75, 10.6 + 6.75 + 53*i), stroke='#e5e5e5', stroke_width=0.264))
+        dwg.add(dwg.line((c.month_line_start_x, c.month_line_start_y + c.month_height*i), (c.timeline_end_x, c.month_line_start_y + c.month_height*i), stroke=c.timeline_line_color, stroke_width=c.timeline_line_width))
 
     # Month labels
     for i in range(4):
         label = str(12-i) + "/22"
-        pos = (1.06, 10.6 + 6.75 + 53*i + 53/2 + 4.23/2)
-        dwg.add(dwg.text(label, insert=pos, font_style='italic', font_size='4.23px', fill='#666666'))
+        pos = (c.month_text_start_x, c.month_line_start_y + c.month_height*i + c.month_height/2 + c.month_text_font_size/2)
+        dwg.add(dwg.text(label, insert=pos, font_style='italic', font_size=str(c.month_text_font_size)+'px', fill=c.month_text_color))
 
     # Date labels
-    dy = 3.7 * 1.5
+    dy = c.date_text_font_size * c.date_text_line_spacing
 
-    text = dwg.text("", insert=(14.58, 10.6 - 3.85), font_style='italic', font_size='3.7px', fill='#808080', text_anchor='end')
+    text = dwg.text("", insert=(c.date_started_start_x, c.date_text_start_y), font_style='italic', font_size=str(c.date_text_font_size)+'px', fill=c.date_text_color, text_anchor=c.date_text_anchor)
     text.add(dwg.tspan('Date'))
-    text.add(dwg.tspan('Started', x=[14.58], dy=[str(dy)+"px"]))
+    text.add(dwg.tspan('Started', x=[c.date_started_start_x], dy=[str(dy)+"px"]))
     dwg.add(text)
 
-    text = dwg.text("", insert=(41.09, 10.6 - 3.85), font_style='italic', font_size='3.7px', fill='#808080', text_anchor='end')
+    text = dwg.text("", insert=(c.date_finished_start_x, c.date_text_start_y), font_style='italic', font_size=str(c.date_text_font_size)+'px', fill=c.date_text_color, text_anchor=c.date_text_anchor)
     text.add(dwg.tspan('Date'))
-    text.add(dwg.tspan('Finished', x=[41.09], dy=[str(dy)+"px"]))
+    text.add(dwg.tspan('Finished', x=[c.date_finished_start_x], dy=[str(dy)+"px"]))
     dwg.add(text)
 
     return dwg
