9cc6245cd9238e880426eae4c59ebbf7d1e48d93
Albert Canales Ros <albertcanalesros@gmail.com>
2022-08-06 12:55:14 +0200

    Made ProcessedBook a class and updated requirements

--- processor.py
+++ processor.py
@@ -3,18 +3,15 @@ from collections import namedtuple
 import calendar
 
 ProcessedMonth = namedtuple("ProcessedMonth", "text text_y line_y")
-ProcessedBook = namedtuple("ProcessedBook", "title subtitle start_y finish_y")
 
 '''
 Represents all the configuration parameters and objects that require some processing before drawn
 '''
 class Processor:
     def __init__(self, data, config):
-
         self._process_months(data, config)
         self._process_books(data, config)
         self._process_other(data, config)
-
 
     # Sets the ProcessedMonth list of the object
     def _process_months(self, data, c):
@@ -30,30 +27,36 @@ class Processor:
             self.months.append(ProcessedMonth(m, text_y, line_y))
             i += 1
 
-    # Sets the ProcessedMonth list of the object
+    # Sets the ProcessedBook list of the object
     def _process_books(self, data, c):
-        self.books = []
-        for book in data.books:
-            subtitle = book.author
-            if book.publication_year is not None:
-                subtitle += " (%d)" % book.publication_year
-            start_y = self._get_y(book.start_date, c)
-            finish_y = self._get_y(book.finish_date, c)
-            self.books.append(ProcessedBook(book.title, subtitle, start_y, finish_y))
-
+        self.books = [ ProcessedBook(book, self.months, c) for book in data.books]
 
     # Sets the rest of parameters that require processing
     def _process_other(self, data, c):
         self.timeline_end_y = c.month_line_start_y + len(self.months) * c.month_height
 
-    # Used by _process_books, to get the tip's y coordinate from its date
-    def _get_y(self, date, c):
-        month_y = self._get_processed_month(date.strftime("%m/%y")).line_y - c.month_height
+class ProcessedBook:
+    def __init__(self, book, months, c):
+        self.title = book.title
+        self.subtitle = self._get_subtitle(book)
+        self.start_y = self._get_y(book.start_date, months, c)
+        self.finish_y = self._get_y(book.finish_date, months, c)
+
+    # Returns the subtitle text for a given book
+    def _get_subtitle(self, book):
+        subtitle = book.author
+        if book.publication_year is not None:
+            subtitle += " (%d)" % book.publication_year
+        return subtitle
+
+    # Returns the tip's y-coordinate for a given date
+    def _get_y(self, date, months, c):
+        month_y = self._get_processed_month(date.strftime("%m/%y"), months).line_y - c.month_height
         month_len = calendar.monthrange(date.year, date.month)[1]
         return month_y + (1 - (int(date.strftime("%d"))-1) / month_len) * c.month_height
 
-    # Used by _process_books, to get the corresponding ProcessedMonth from a given already formatted date
-    def _get_processed_month(self, text):
-        month = [m for m in self.months if m.text == text]
+    # Returns the corresponding ProcessedMonth from a given date (already formatted)
+    def _get_processed_month(self, text, months):
+        month = [m for m in months if m.text == text]
         assert month, "No ProcessedMonth found for date %s" % text
         return month[0]
