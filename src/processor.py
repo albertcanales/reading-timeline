5956bd09f5f9eb1b8d870684815e889553775a72
Albert Canales Ros <albertcanalesros@gmail.com>
2024-08-27 15:12:39 +0200

    Rename variables still referencing config to params

--- src/processor.py
+++ src/processor.py
@@ -21,7 +21,7 @@ class Processor:
         self._process_other(data, params)
 
     # Sets the ProcessedMonth list of the object
-    def _process_months(self, data, c):
+    def _process_months(self, data, prms):
         min_date, max_date = data.get_min_date(data.books), data.get_max_date(
             data.books
         )
@@ -30,53 +30,59 @@ class Processor:
         )
         if int(min_date.strftime("%d")) > 1:
             months_text = [min_date.strftime("%m/%y")] + months_text
-        if c.reverse_timeline:
+        if prms.reverse_timeline:
             months_text = months_text[::-1]
         i = 0
         self.months = []
         for m in months_text:
             text_y = (
-                c.month_line_start_y
-                + c.month_height * i
-                + c.month_height / 2
-                + c.month_text_font_size / 2
+                prms.month_line_start_y
+                + prms.month_height * i
+                + prms.month_height / 2
+                + prms.month_text_font_size / 2
             )
-            line_y = c.month_line_start_y + c.month_height * (i + 1)
+            line_y = prms.month_line_start_y + prms.month_height * (i + 1)
             self.months.append(ProcessedMonth(m, text_y, line_y))
             i += 1
 
     # Sets the ProcesseCategory list of the object
-    def _process_categories(self, data, c):
-        y = c.category_start_y
+    def _process_categories(self, data, prms):
+        y = prms.category_start_y
         self.categories = []
         used_categories = {book.category for book in data.books}
         for category in data.categories:
-            if not c.category_hide_unused or category in used_categories:
+            if not prms.category_hide_unused or category in used_categories:
                 self.categories.append(
                     ProcessedCategory(category.name, category.color, y)
                 )
-                y += c.category_vertical_spacing
+                y += prms.category_vertical_spacing
 
     # Sets the ProcessedBook list of the object
-    def _process_books(self, data, c):
-        self.books = [ProcessedBook(book, self.months, c) for book in data.books]
-        self._check_book_collisions(c)
+    def _process_books(self, data, prms):
+        self.books = [ProcessedBook(book, self.months, prms) for book in data.books]
+        self._check_book_collisions(prms)
 
     # Sets the rest of parameters that require processing
-    def _process_other(self, data, c):
-        self.timeline_end_y = c.month_line_start_y + len(self.months) * c.month_height
-        self.category_line_end_x = c.category_line_start_x + c.category_line_length
-        self.category_text_start_x = self.category_line_end_x + c.category_text_padding
+    def _process_other(self, data, prms):
+        self.timeline_end_y = (
+            prms.month_line_start_y + len(self.months) * prms.month_height
+        )
+        self.category_line_end_x = (
+            prms.category_line_start_x + prms.category_line_length
+        )
+        self.category_text_start_x = (
+            self.category_line_end_x + prms.category_text_padding
+        )
         self.canvas_size_y = (
-            self.timeline_end_y + c.canvas_padding_bottom * c.canvas_zoom
+            self.timeline_end_y + prms.canvas_padding_bottom * prms.canvas_zoom
         )
 
-    def _check_book_collisions(self, c):
+    def _check_book_collisions(self, prms):
         # Find minimum separation between starting tips
         min_y = self._get_min_separation(lambda x: x.start_y)
 
-        if min_y < 2 * c.book_tip_radius:
-            new_min_y = 2 * c.book_tip_radius * c.month_height / min_y
+        if min_y < 2 * prms.book_tip_radius:
+            new_min_y = 2 * prms.book_tip_radius * prms.month_height / min_y
             log.warning(
                 (
                     "Book tips are colliding. To prevent it, you may:\n"
@@ -90,11 +96,12 @@ class Processor:
         # Find minimum separation between book labels
         min_y = self._get_min_separation(lambda x: x.finish_y)
         text_height = (
-            c.book_title_font_size * c.book_text_line_spacing + c.book_author_font_size
+            prms.book_title_font_size * prms.book_text_line_spacing
+            + prms.book_author_font_size
         )
 
         if min_y < text_height:
-            new_min_y = text_height * c.month_height / min_y
+            new_min_y = text_height * prms.month_height / min_y
             log.warning(
                 (
                     "Book labels are colliding. To prevent it, you may:\n"
@@ -118,22 +125,22 @@ class Processor:
 
 
 class ProcessedBook:
-    def __init__(self, book, months, c):
+    def __init__(self, book, months, prms):
         self.title = book.title
         self.subtitle = self._get_subtitle(book)
-        self.start_y = self._get_y(book.start_date, months, c)
-        self.finish_y = self._get_y(book.finish_date, months, c)
-        self.color = c.category_default_color
+        self.start_y = self._get_y(book.start_date, months, prms)
+        self.finish_y = self._get_y(book.finish_date, months, prms)
+        self.color = prms.category_default_color
         if book.category is not None:
             self.color = book.category.color
         self.score = None
         if book.score is not None:
             self.score = book.score / 2
-            self.score_x = c.book_text_start_x + self._get_textwidth(
-                self.subtitle, c.book_author_font_size, c.book_text_font
+            self.score_x = prms.book_text_start_x + self._get_textwidth(
+                self.subtitle, prms.book_author_font_size, prms.book_text_font
             )
             self.score_color = (
-                self.color if c.book_score_colored else c.book_score_color
+                self.color if prms.book_score_colored else prms.book_score_color
             )
         self.link = book.link
 
@@ -145,16 +152,16 @@ class ProcessedBook:
         return subtitle
 
     # Returns the tip's y-coordinate for a given date
-    def _get_y(self, date, months, c):
+    def _get_y(self, date, months, prms):
         month_y = (
             self._get_processed_month(date.strftime("%m/%y"), months).line_y
-            - c.month_height
+            - prms.month_height
         )
         month_len = monthrange(date.year, date.month)[1]
         month_prop = (int(date.strftime("%d")) - 1) / month_len
-        if c.reverse_timeline:
+        if prms.reverse_timeline:
             month_prop = 1 - month_prop
-        return month_y + month_prop * c.month_height
+        return month_y + month_prop * prms.month_height
 
     # Returns the corresponding ProcessedMonth from a given date (already formatted)
     def _get_processed_month(self, text, months):
